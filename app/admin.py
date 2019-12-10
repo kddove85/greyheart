04bfb251075367fa9ead1f990c2a3bd759ac75e9
Kyle Dove <kddove85@gmail.com>
2019-12-10 15:21:39 -0500

    GH-3 Integrated Cloudinary

--- app/admin.py
+++ app/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
 from .models import Greyhound
 from .models import BoardMember
+from cloudinary import uploader
+
+
+def delete_model(modeladmin, request, queryset):
+    for obj in queryset:
+        uploader.destroy(obj.profile_image.public_id)
+        obj.delete()
 
 
 # Register your models here.
 class GreyhoundAdmin(admin.ModelAdmin):
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
     fields = ['name',
               'bio',
               'is_available',
@@ -14,11 +28,21 @@ class GreyhoundAdmin(admin.ModelAdmin):
               'is_spotlight',
               'profile_image']
     list_display = ('name',)
+    actions = [delete_model]
 
 
 class BoardMemberAdmin(admin.ModelAdmin):
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
     fields = ['name', 'title', 'bio', 'profile_image']
     list_display = ('name',)
+
+    actions = [delete_model]
 
 
 admin.site.register(Greyhound, GreyhoundAdmin)
--- app/models.py
+++ app/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db.models import ImageField
 import os
 from cloudinary.models import CloudinaryField
 
